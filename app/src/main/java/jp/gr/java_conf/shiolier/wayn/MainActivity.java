1ed5d63a34ed03f3647f58ede00fb6c30c13209f
Haruto Tanaka <dev.htanaka@gmail.com>
2015-02-22 14:00:23 +0900

    #1 MainActivity変更

--- app/src/main/java/jp/gr/java_conf/shiolier/wayn/MainActivity.java
+++ app/src/main/java/jp/gr/java_conf/shiolier/wayn/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,28 +14,29 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
+		Button btnRadar = (Button)findViewById(R.id.btn_radar);
+		btnRadar.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+			}
+		});
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+		Button btnPostLocation = (Button)findViewById(R.id.btn_post_location);
+		btnPostLocation.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+			}
+		});
 
-		return super.onOptionsItemSelected(item);
+		Button btnSetting = (Button)findViewById(R.id.btn_setting);
+		btnSetting.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
 	}
 }
