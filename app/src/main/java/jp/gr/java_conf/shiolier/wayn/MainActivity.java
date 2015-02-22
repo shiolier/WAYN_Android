392c58e7f5b486b63306666d4e732e5d3dc8f479
Haruto Tanaka <cavalier0203@gmail.com>
2015-02-22 23:48:19 +0900

    Revert "#10 位置情報送信Service実装"

--- app/src/main/java/jp/gr/java_conf/shiolier/wayn/MainActivity.java
+++ app/src/main/java/jp/gr/java_conf/shiolier/wayn/MainActivity.java
@@ -1,131 +1,39 @@
 package jp.gr.java_conf.shiolier.wayn;
 
-import android.app.ActivityManager;
-import android.content.Intent;
-import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import java.util.List;
-import java.util.UUID;
-
-import jp.gr.java_conf.shiolier.wayn.asynctask.UserRegistAsyncTask;
-import jp.gr.java_conf.shiolier.wayn.entity.User;
-import jp.gr.java_conf.shiolier.wayn.service.PostLocationService;
-import jp.gr.java_conf.shiolier.wayn.util.MySharedPref;
 
 public class MainActivity extends ActionBarActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
 
-		Button btnRadar = (Button)findViewById(R.id.btn_radar);
-		btnRadar.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (!isPostLocationServiceRunning()) {
-					runPostLocationService();
-				}
 
-				Intent intent = new Intent(MainActivity.this, RadarActivity.class);
-				startActivity(intent);
-			}
-		});
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
 
-		ToggleButton tglbtnPostLocation = (ToggleButton)findViewById(R.id.tglbtn_post_location_enable);
-		if (isPostLocationServiceRunning()) {
-			tglbtnPostLocation.setChecked(true);
-		} else {
-			tglbtnPostLocation.setChecked(false);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
 		}
-		tglbtnPostLocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if (isChecked) {
-					runPostLocationService();
-				} else {
-					stopPostLocationService();
-				}
-			}
-		});
 
-		Button btnSetting = (Button)findViewById(R.id.btn_setting);
-		btnSetting.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, SettingActivity.class);
-				startActivity(intent);
-			}
-		});
-
-		MySharedPref mySharedPref = new MySharedPref(this);
-		if (mySharedPref.getUserId(0) == 0) {
-			regist();
-		}
-		Log.i("MyLog", String.format("User ID: %d", mySharedPref.getUserId(0)));
-		Log.i("MyLog", String.format("User Password: %s", mySharedPref.getUserPassword("")));
-	}
-
-	private void regist() {
-		UserRegistAsyncTask asyncTask = new UserRegistAsyncTask(this, new UserRegistAsyncTask.OnPostExecuteListener() {
-			@Override
-			public void onPostExecute(User user) {
-				if (user.getId() != 0) {
-					MySharedPref mySharedPref = new MySharedPref(MainActivity.this);
-					mySharedPref.setUserId(user.getId());
-				} else {
-					registFailure();
-				}
-			}
-		});
-
-		String password = UUID.randomUUID().toString();
-		MySharedPref mySharedPref = new MySharedPref(MainActivity.this);
-		mySharedPref.setUserPassword(password);
-
-		asyncTask.execute("SampleName", password);
-	}
-
-	private void registFailure() {
-		Toast.makeText(this, "初期化に失敗しました。", Toast.LENGTH_SHORT).show();
-		finish();
-	}
-
-	private boolean isPostLocationServiceRunning() {
-		String serviceName = PostLocationService.class.getSimpleName();
-
-		ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-		List<ActivityManager.RunningServiceInfo> serviceInfos = activityManager.getRunningServices(Integer.MAX_VALUE);
-		if (serviceInfos != null) {
-			for(ActivityManager.RunningServiceInfo serviceInfo : serviceInfos){
-				if(serviceInfo.service.getClassName().endsWith(serviceName)){
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	private void runPostLocationService() {
-		Intent intent = new Intent(this, PostLocationService.class);
-		startService(intent);
-
-		ToggleButton tglbtnPostLocation = (ToggleButton)findViewById(R.id.tglbtn_post_location_enable);
-		tglbtnPostLocation.setChecked(true);
-	}
-
-	private void stopPostLocationService() {
-		Intent intent = new Intent(this, PostLocationService.class);
-		stopService(intent);
-
-		ToggleButton tglbtnPostLocation = (ToggleButton)findViewById(R.id.tglbtn_post_location_enable);
-		tglbtnPostLocation.setChecked(false);
+		return super.onOptionsItemSelected(item);
 	}
 }
